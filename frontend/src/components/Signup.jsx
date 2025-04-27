04026ace9b39e1c590b5f5db80b9afb0b1fb9351
YASHRAJ MAURYA <yashrajmaurya1234@gmail.com>
2025-04-27 12:28:01 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
     const submitHandler = async(e) => {
         e.preventDefault()
         try {
-            const res = await axios.post("http://localhost:8000/api/v1/user/register", input, {
+            const res = await axios.post("https://g-mail-backend.onrender.com/api/v1/user/register", input, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
