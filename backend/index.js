a7f56a17942980ec38abb2172994cb989a573b67
YASHRAJ MAURYA <yashrajmaurya1234@gmail.com>
2025-04-27 12:42:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use(cors({origin: *, credentials: true}))
 
 //routes
 app.use("/api/v1/user", userRoute)
